969e9b340be37f4e63e34bbc73c00eba1a6feeb0
Cherrybael <...>
2024-10-11 00:22:55 +0300

    fixed incorrect info.json

--- planner_wrap.py
+++ planner_wrap.py
@@ -7,9 +7,10 @@ def get_marks():
 def exec_distribution(blacklist, problem_numbers, rand_seed):
     marks = google_sheet_to_json("https://docs.google.com/spreadsheets/d/1yNata06mQnIxJ4y5Ad936cDFiUvH2SToFn1nfkjrswc", "marks.json")
     marks = {x:int(y) for x,y in marks.items()}
+    marks_backup = marks.copy()
     preferences = generate_entry("tasks.json") 
     plr = Planner(marks, preferences, blacklist, problem_numbers, rand_seed)
     ans = plr.distribute_problems()
     for x in list(ans.keys()):
         print(x, ans[x])
-    return ans, marks, preferences, blacklist, rand_seed
+    return ans, marks_backup, preferences, blacklist, rand_seed
